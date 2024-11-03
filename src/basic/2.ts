c989b10ea696103b15c7d31dbebf9eb3e3d61218
Oleksandr-Korniienko514 <bon.bon199993@gmail.com>
2024-11-03 17:10:43 +0100

    basik completed

---
+++ src/basic/2.ts
@@ -0,0 +1,23 @@
+// У вас є наступний JavaScript масив:
+
+// let person = ['Max', 21];
+
+// Як переписати його в TypeScript, використовуючи концепцію кортежів, щоб гарантувати, що перший елемент завжди буде рядком, а другий числом ?
+
+
+// ---------------------------------------------------
+
+
+// !варіант 1 let person: (number | string)[] = ['Max', 21];
+
+//!варіант 2  let person: [string, number] = ['Max', 21]; ???
+
+
+function getPerson(): (number | string)[] {
+    let person: (number | string)[] = ['Max', 21];
+    return person;
+}
+
+const personArray = getPerson();
+console.log(personArray);
+
---
+++ src/basic/6.ts
@@ -0,0 +1,45 @@
+// const mango = {
+//     name: 'Mango',
+//     age: 30,
+//     email: 'john@example.com',
+//     address: {
+//         city: 'New York',
+//         country: 'USA'
+//     }
+// };
+
+// const poly = {
+//     name: 'Mango',
+//     age: 30,
+//     email: 'john@example.com'
+// };
+
+// ---------------------------------------------------
+
+interface Address {
+    city: string;
+    country: string;
+}
+
+interface User {
+    name: string;
+    age: number;
+    email: string;
+    address?: Address;
+}
+
+const mango: User = {
+    name: 'Mango',
+    age: 30,
+    email: 'john@example.com',
+    address: {
+        city: 'New York',
+        country: 'USA'
+    }
+};
+
+const poly: User = {
+    name: 'Poly',
+    age: 30,
+    email: 'poly@example.com'
+};
